f79d5b6d427eee8deec24a87516f81caf017c3df
Ewus <ewus9999@gmail.com>
2022-05-31 12:57:26 +0200

    deleteInsurances i updateInsurance

--- src/main/java/eb/study/springstudy/controller/InsuranceController.java
+++ src/main/java/eb/study/springstudy/controller/InsuranceController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -30,8 +31,13 @@ public class InsuranceController {
         return ResponseEntity.ok(insuranceService.getInsurances());
     }
 
-    @PostMapping("/updateInsurance")
-    public void updateInsurance( @RequestBody InsuranceDto newInsuranceDto){
-        insuranceService.updateInsuranceExpirationAndStartDate(newInsuranceDto);
+    @PostMapping("/updateInsurance/{id1}/{id2}")
+    public void updateInsurance( @RequestBody InsuranceDto newInsuranceDto, @PathVariable("id1") Integer id1, @PathVariable("id2") Integer id2){
+        insuranceService.updateInsuranceExpirationAndStartDate(newInsuranceDto, id1, id2);
+    }
+
+    @DeleteMapping("/deleteInsurances/{id1}/{id2}")
+    public void deleteInsurances(@PathVariable("id1") Integer id1, @PathVariable("id2") Integer id2){
+        insuranceService.deleteInsurances(id1, id2);
     }
 }
